92b62c9eebb5e6ee84b7675d13353a1d00b573c7
Hsin Ling Hu <hsinkava11@gmail.com>
2020-03-07 20:48:23 -0800

    completed

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,9 +1,11 @@
 import config from '../config'
+import TokenService from '../services/token-service'
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
       headers: {
+        
       },
     })
       .then(res =>
@@ -15,6 +17,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -26,6 +29,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
+        'authorization':`basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -35,10 +39,12 @@ const ThingApiService = {
       )
   },
   postReview(thingId, text, rating) {
+    
     return fetch(`${config.API_ENDPOINT}/reviews`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         thing_id: thingId,
